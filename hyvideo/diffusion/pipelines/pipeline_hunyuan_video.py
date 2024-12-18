adf135397f7ac20e7f845caad3d54c92ebf5bee2
Gothos <vishnujaddipal@gmail.com>
2024-12-18 12:25:26 +0530

    remove prints

--- hyvideo/diffusion/pipelines/pipeline_hunyuan_video.py
+++ hyvideo/diffusion/pipelines/pipeline_hunyuan_video.py
@@ -505,7 +505,6 @@ class HunyuanVideoPipeline(DiffusionPipeline):
     def get_timesteps(self, num_inference_steps, timesteps, strength, device):
         # get the original timestep using init_timestep
         init_timestep = min(int(num_inference_steps * strength), num_inference_steps)
-        print(init_timestep,"init_timestep")
         t_start = max(num_inference_steps - init_timestep, 0)
         timesteps = timesteps[t_start * self.scheduler.order :]
 
@@ -629,7 +628,6 @@ class HunyuanVideoPipeline(DiffusionPipeline):
                     else:
                         init_latents = [retrieve_latents(self.vae.encode(vid.unsqueeze(0)), generator) for vid in video]
                     init_latents = torch.cat(init_latents, dim=0).to(dtype) #.permute(0, 2, 1, 3, 4)  # [B, F, C, H, W]
-                    print(init_latents.shape,"ini shape")
                     if (
                         hasattr(self.vae.config, "shift_factor")
                         and self.vae.config.shift_factor
@@ -641,11 +639,9 @@ class HunyuanVideoPipeline(DiffusionPipeline):
                     else:
                         init_latents = init_latents * self.vae.config.scaling_factor
                     noisy_init_latents = self.scheduler.scale_noise(init_latents, timestep,noise)
-                    print(noisy_init_latents.shape,noise.shape,timestep,"noisy, noise")
                     
 
                     latents = noisy_init_latents
-                    print(latents.shape,"lat shape")
 
                 else:
                     latents = noise
@@ -981,10 +977,7 @@ class HunyuanVideoPipeline(DiffusionPipeline):
         if video is not None:
             video = self.video_processor.preprocess_video(video, height=height, width=width)
             video = video.to(device=device, dtype=prompt_embeds.dtype)
-            print(timesteps,num_inference_steps,strength,"before")
             timesteps, num_inference_steps= self.get_timesteps(num_inference_steps, timesteps, strength, device)
-            print(timesteps,num_inference_steps,"after")
-            print(timesteps[:1].repeat(batch_size * num_videos_per_prompt),"repeat")
         noise_timestep = timesteps[:1].repeat(batch_size * num_videos_per_prompt)
 
         if "884" in vae_ver:
@@ -1008,7 +1001,6 @@ class HunyuanVideoPipeline(DiffusionPipeline):
             generator,
             latents,noise_timestep
         )
-        print(latents.shape,noise.shape,video_latents.shape,"3 shapes")
 
 
         # 6. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
